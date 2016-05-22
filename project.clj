d315a5f83a2770aba7b6f6a6a943a7f0c8fcfdd3
Ryo Akagi <ryoakg@gmail.com>
2016-05-22 20:22:46 +0900

    add group-id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-desktop-dialog "0.1.0-SNAPSHOT"
+(defproject ryo/clj-desktop-dialog "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
